a16fd7767c28d0f71141eb288f81589171e53716
Elijah <elijahoneal@yahoo.com>
2021-03-21 11:58:58 -0400

    Form Renders during tests and passes each test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render , screen} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>)
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm/>);
+
+    const firstNameInput = screen.getByLabelText(/first name/i)
+    const lastNameInput = screen.getByLabelText(/last name/i)
+    const addressInput = screen.getByLabelText(/address/i)
+    const cityInput = screen.getByLabelText(/city/i)
+    const stateInput = screen.getByLabelText(/state/i)
+    const zipInput = screen.getByLabelText(/zip/i)
+
+    userEvent.type( firstNameInput , "Pamela")
+    userEvent.type( lastNameInput , "Isley")
+    userEvent.type( addressInput , "181 Blossom Ave")
+    userEvent.type( cityInput , "Gotham")
+    userEvent.type( stateInput , "DC")
+    userEvent.type( zipInput , "61966")
+
+    const checkoutBtn = screen.getByRole('button');
+    userEvent.click(checkoutBtn);
+
+    const successMessage = screen.getByText("You have ordered some plants! Woo-hoo!");
+    
+    expect(successMessage).toBeDefined();
+    expect("Pamela Isley").toBeDefined();
+    expect("181 Blossom Ave").toBeDefined();
+    expect("Gotham, DC 61966").toBeDefined();
+});
